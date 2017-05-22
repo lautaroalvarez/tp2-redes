c7c46cb4e56e615a9e898982c5fd4f2db1ef2f01
matias <matias@la-maquina-de-dios>
2017-05-22 02:12:14 -0300

    agrego la geolocalizacion de las ips, hay que buscar una biblioteca para pasar latitud y longitud a un mapa

--- source/traceroute.py
+++ source/traceroute.py
@@ -3,12 +3,21 @@ import os, sys, time, csv
 
 CANT_REP = 5
 TYPE_TIMEEXCEDED = 11
+LINEA_PAIS = 468
+LINEA_REGION = 479
+LINEA_CIUDAD = 484
+LINEA_LATITUD = 501
+LINEA_LONGITUD =506
+
+def dameLinea(numLinea):
+    return os.popen("head -n"+str(numLinea)+" dummy | tail -n1 | sed 's/<span>//g' | sed 's/<\/span>//g' | sed 's/\t//g'").read()[:-1]
 
 # Recibe un string con la ip/url de destino y un ttl maximo del paquete (para que el algortimo termine)
 def traceroute(destino, ttlMaximo, outfile):
     route = []
     if outfile:
         logger = csv.writer(open(outfile + ".csv", "wb"))
+        logger.writerow(["orden","ip","avg(rtt)","pais","region","ciudad","latitud","longitud"])
 
     for i in range(1, ttlMaximo + 1):
         host = {}
@@ -36,10 +45,23 @@ def traceroute(destino, ttlMaximo, outfile):
 
     i = 1
     for host in route:
+        if host['ip'][:7] != "192.168":
+            # si el equipo es externo traigo datos de geolocalizacion poniendo la ip
+            os.system("wget -O dummy https://geoiptool.com/es/?ip="+host['ip'])
+        else:
+            # si es el router interno le digo a la pagina que me rastree
+            os.system("wget -O dummy https://geoiptool.com/")
+        pais = dameLinea(LINEA_PAIS)
+        region = dameLinea(LINEA_REGION)
+        ciudad = dameLinea(LINEA_CIUDAD)
+        latitud = dameLinea(LINEA_LATITUD)
+        longitud = dameLinea(LINEA_LONGITUD)
         if outfile:
-            logger.writerow([i, host['ip'], host['rtt']])
+            logger.writerow([i, host['ip'], host['rtt'],pais,region,ciudad,latitud,longitud])
         print host['ip'] + " -> " + str(host['rtt'])
         i += 1
+    # borro el archivo que baje
+    os.system("rm dummy*")
 
 if os.geteuid() != 0:
     print "Correlo con root chabon!"
