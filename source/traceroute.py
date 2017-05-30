3a1baa147f47f23093232031e216d3967b137759
Lautaro <laucha@laucha-pc>
2017-05-30 19:05:48 -0300

    Agrego algunas verificaciones y números de intento para que no se cuelgue. Agrego en ideas algo copado para sacar la geolocalización de la ip.

--- source/traceroute.py
+++ source/traceroute.py
@@ -1,8 +1,11 @@
 from scapy.all import *
-import os, sys, time, csv
+import os, sys, time, csv, geoip2.webservice
 
 CANT_REP = 5
+MAX_INTENTOS = 5
+TIME_LIMIT = 3
 TYPE_TIMEEXCEDED = 11
+TYPE_LASTNODE = 0
 LINEA_PAIS = 468
 LINEA_REGION = 479
 LINEA_CIUDAD = 484
@@ -17,49 +20,67 @@ def traceroute(destino, ttlMaximo, outfile):
     route = []
     if outfile:
         logger = csv.writer(open(outfile + ".csv", "wb"))
-        logger.writerow(["orden","ip","avg(rtt)","pais","region","ciudad","latitud","longitud"])
+        logger.writerow(["orden","ip","num_rep","pais","region","ciudad","latitud","longitud"])
 
-    for i in range(1, ttlMaximo + 1):
+    ttlActual = 1
+    fin = 0
+    last_ip = ''
+    while ttlActual <= ttlMaximo and not fin:
         host = {}
-        time_sum = 0
+        host['tiempos'] = []
+        host['ttl'] = ttlActual
         num_rep = 0
-        while num_rep < CANT_REP:
+        num_intentos = 0
+        while num_rep < CANT_REP and num_intentos < MAX_INTENTOS and not fin:
+            print "-----------------------"
+            print "----ttl: " + str(ttlActual) + " - num_rep: " + str(num_rep) + " - num_intentos: " + str(num_intentos)
             # arma el paquete con el ttl incluido
-            paquete = IP(dst=destino,ttl=i)/ICMP()
+            paquete = IP(dst=destino,ttl=ttlActual)/ICMP()
             # hago un echo request o ping para los pibes
             start_time = time.time()
-            response = sr1(paquete)
+            response = sr1(paquete, timeout=TIME_LIMIT)
             end_time = time.time()
-            # analizo response
-            if response[ICMP].type == TYPE_TIMEEXCEDED:
-                # fue time exceded
-                # guarda ip del host al que llego
-                host['ip'] = response.src
-                # se suma el rtt para promediar
-                time_sum += end_time - start_time
-                # aumenta contador
-                num_rep += 1
-        # se guarda el rtt promedio
-        host['rtt'] = time_sum / CANT_REP
-        route.append(host)
+            num_intentos += 1
+            if response:
+                # analizo response
+                # si la ip es la misma que un ttl anterior, entonces ya llegue al final
+                if last_ip == response.src:
+                    print "========================REPITE LAST IP!!========================="
+                    fin = 1
+                else:
+                    print "type: " + str(response[ICMP].type)
+                    print "host: " + str(response.src)
+                    if response[ICMP].type == TYPE_TIMEEXCEDED or response[ICMP].type == TYPE_LASTNODE:
+                        print "se guarda ok"
+                        # fue time exceded
+                        # guarda ip del host al que llego
+                        host['ip'] = response.src
+                        # guarda el tiempo obtenido en el host
+                        host['tiempos'].append(end_time - start_time)
+                        # aumenta contador
+                        num_rep += 1
+                        #reseteamos el numero de intentos
+                        num_intentos = 0
+        if len(host['tiempos']) > 0:
+            last_ip = host['ip']
+            route.append(host)
+        ttlActual += 1
 
-    i = 1
     for host in route:
-        if host['ip'][:7] != "192.168":
+        if host['ip'][:7] == "192.168" or host['ip'][:3] == "10." or host['ip'][:7] == "192.16." or host['ip'][:7] == "169.254":
+            # si es de red interna le digo a la pagina que me rastree
+            os.system("wget -O dummy https://geoiptool.com/")
+        else:
             # si el equipo es externo traigo datos de geolocalizacion poniendo la ip
             os.system("wget -O dummy https://geoiptool.com/es/?ip="+host['ip'])
-        else:
-            # si es el router interno le digo a la pagina que me rastree
-            os.system("wget -O dummy https://geoiptool.com/")
         pais = dameLinea(LINEA_PAIS)
         region = dameLinea(LINEA_REGION)
         ciudad = dameLinea(LINEA_CIUDAD)
         latitud = dameLinea(LINEA_LATITUD)
         longitud = dameLinea(LINEA_LONGITUD)
         if outfile:
-            logger.writerow([i, host['ip'], host['rtt'],pais,region,ciudad,latitud,longitud])
-        print host['ip'] + " -> " + str(host['rtt'])
-        i += 1
+            for rtt in host['tiempos']:
+                logger.writerow([host['ttl'], host['ip'], rtt, pais, region, ciudad, latitud, longitud])
     # borro el archivo que baje
     os.system("rm dummy*")
 
