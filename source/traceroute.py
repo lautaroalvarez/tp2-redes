62b9c80bd65c94fe02e135b5f819780bf9000d2d
Lautaro <laucha@laucha-pc>
2017-06-02 20:39:04 -0300

    Saqué ttlMaximo

--- source/traceroute.py
+++ source/traceroute.py
@@ -3,30 +3,29 @@ import os, sys, time, csv
 import json, requests
 import numpy as np
 
-CANT_REP = 30
-MAX_INTENTOS = 5
+CANT_REP = 3
+MAX_INTENTOS = 2
 TIME_LIMIT = 3
 TYPE_TIMEEXCEDED = 11
 TYPE_LASTNODE = 0
 
 # Recibe un string con la ip/url de destino y un ttl maximo del paquete (para que el algortimo termine)
-def traceroute(destino, ttlMaximo, outfile):
+def traceroute(destino, outfile):
     route = []
     if not outfile:
         outfile = "salida" + str(time.time())
     logger = csv.writer(open(outfile + ".csv", "wb"))
-    logger.writerow(["orden","ip","num_rep","pais","region","ciudad","latitud","longitud"])
+    logger.writerow(["orden","ip","num_rep","pais","region","ciudad","latitud","longitud","tiempo"])
 
     ttlActual = 1
-    fin = 0
-    last_ip = ''
-    while ttlActual <= ttlMaximo and not fin:
+    fin_camino = 0
+    while not fin_camino:
         host = {}
         host['tiempos'] = []
         host['ttl'] = ttlActual
         num_rep = 0
         num_intentos = 0
-        while num_rep < CANT_REP and num_intentos < MAX_INTENTOS and not fin:
+        while num_rep < CANT_REP and num_intentos < MAX_INTENTOS:
             print "----ttl: " + str(ttlActual) + " - num_rep: " + str(num_rep) + " - num_intentos: " + str(num_intentos)
             # arma el paquete con el ttl incluido
             paquete = IP(dst=destino,ttl=ttlActual)/ICMP()
@@ -37,28 +36,24 @@ def traceroute(destino, ttlMaximo, outfile):
             num_intentos += 1
             if response:
                 # analizo response
-                # si la ip es la misma que un ttl anterior, entonces ya llegue al final
-                if last_ip == response.src:
-                    print "========================REPITE IP================="
-                    # fin = 1
-                else:
-                    print "contacto host: " + str(response.src)
-                    if response[ICMP].type == TYPE_TIMEEXCEDED or response[ICMP].type == TYPE_LASTNODE:
-                        # fue time exceded
-                        # guarda ip del host al que llego
-                        host['ip'] = response.src
-                        # guarda el tiempo obtenido en el host
-                        host['tiempos'].append(end_time - start_time)
-                        # aumenta contador
-                        num_rep += 1
-                        #reseteamos el numero de intentos
-                        num_intentos = 0
+                if response[ICMP].type == TYPE_TIMEEXCEDED or response[ICMP].type == TYPE_LASTNODE:
+                    # guarda ip del host al que llego
+                    host['ip'] = response.src
+                    # guarda el tiempo obtenido en el host
+                    host['tiempos'].append(end_time - start_time)
+                    # aumenta contador
+                    num_rep += 1
+                    # reseteamos el numero de intentos
+                    num_intentos = 0
+                # chequeamos si es el nodo de destino
+                if response[ICMP].type == TYPE_LASTNODE:
+                    fin_camino = 1
         if len(host['tiempos']) > 0:
             last_ip = host['ip']
             route.append(host)
         ttlActual += 1
 
-    ej1 = False
+    ej1 = True
     arrayIP_RTT = []
     for host in route:
         # si es de red interna le digo a la pagina que me rastree
@@ -139,12 +134,11 @@ if os.geteuid() != 0:
     print "Correlo con root chabon!"
     exit(1)
 
-if len(sys.argv) > 3:
+if len(sys.argv) > 2:
     host = sys.argv[1]
-    ttlMax = sys.argv[2]
-    outfile = sys.argv[3]
+    outfile = sys.argv[2]
 else:
     host = raw_input("Pone la ip/url: ")
-    ttlMax = int(raw_input("Pone el ttl maximo: "))
     outfile = raw_input("Pone el archivo de salida de logs: ")
-traceroute(host, ttlMax, outfile)
+
+traceroute(host, outfile)
