3eb60286a77fb02d4405c161311c6b469a302c42
Lautaro <laucha@laucha-pc>
2017-06-03 01:00:44 -0300

    Agrego cambios para ver mejor el avance del traceroute. Agrego dos modos, uno para capturar y otro para sacar el outlier.

--- source/traceroute.py
+++ source/traceroute.py
@@ -4,100 +4,143 @@ import json, requests
 import numpy as np
 
 CANT_REP = 50
-MAX_INTENTOS = 2
+MAX_INTENTOS = 3
 TIME_LIMIT = 3
+NODES_LIMIT = 30
 TYPE_TIMEEXCEDED = 11
 TYPE_LASTNODE = 0
 
-# Recibe un string con la ip/url de destino y un ttl maximo del paquete (para que el algortimo termine)
-def traceroute(destino, outfile):
-    route = []
-    if not outfile:
-        outfile = "salida" + str(time.time())
-    logger = csv.writer(open(outfile + ".csv", "wb"))
-    logger.writerow(["orden","ip","num_rep","pais","region","ciudad","latitud","longitud","tiempo"])
+# Recibe un string con la ip/url de destino y un archivo de salida
+class traceroute():
+    def __init__(self, destino, namefile):
+        if not namefile:
+            namefile = str(time.time())
+        self.namefile = namefile
+        self.destino = destino
+        self.route = []
+        self.numRep = 0
+        self.numIntento = 0
 
-    ttlActual = 1
-    fin_camino = 0
-    while not fin_camino:
-        host = {}
-        host['tiempos'] = []
-        host['ttl'] = ttlActual
-        num_rep = 0
-        num_intentos = 0
-        while num_rep < CANT_REP and num_intentos < MAX_INTENTOS:
-            print "----ttl: " + str(ttlActual) + " - num_rep: " + str(num_rep) + " - num_intentos: " + str(num_intentos)
-            # arma el paquete con el ttl incluido
-            paquete = IP(dst=destino,ttl=ttlActual)/ICMP()
-            # hago un echo request o ping para los pibes
-            start_time = time.time()
-            response = sr1(paquete, timeout=TIME_LIMIT)
-            end_time = time.time()
-            num_intentos += 1
-            if response:
-                # analizo response
-                if response[ICMP].type == TYPE_TIMEEXCEDED or response[ICMP].type == TYPE_LASTNODE:
-                    # guarda ip del host al que llego
-                    host['ip'] = response.src
-                    # guarda el tiempo obtenido en el host
-                    host['tiempos'].append(end_time - start_time)
-                    # aumenta contador
-                    num_rep += 1
-                    # reseteamos el numero de intentos
-                    num_intentos = 0
-                # chequeamos si es el nodo de destino
-                if response[ICMP].type == TYPE_LASTNODE:
-                    fin_camino = 1
-        if len(host['tiempos']) > 0:
-            last_ip = host['ip']
-            route.append(host)
-        ttlActual += 1
+    def iniciar(self):
+        self.loggerRoute = csv.writer(open(self.namefile + "_route.csv", "wb"))
+        self.loggerRoute.writerow(["ttl","ip","rtt","pais","region","ciudad","latitud","longitud"])
+        self.ttlActual = 1
+        fin_camino = False
+        while not fin_camino and self.ttlActual < NODES_LIMIT:
+            host = {}
+            host['tiempos'] = []
+            host['ttl'] = self.ttlActual
+            self.numRep = 0
+            numIntento = 0
+            while self.numRep < CANT_REP and numIntento < MAX_INTENTOS:
+                self.actualizarVistaProceso()
+                # arma el paquete con el ttl incluido
+                paquete = IP(dst=self.destino,ttl=self.ttlActual)/ICMP()
+                # hago un echo request o ping para los pibes
+                start_time = time.time()
+                response = sr1(paquete, timeout=TIME_LIMIT)
+                end_time = time.time()
+                numIntento += 1
+                if response:
+                    # analizo response
+                    if response[ICMP].type == TYPE_TIMEEXCEDED or response[ICMP].type == TYPE_LASTNODE:
+                        # guarda ip del host al que llego
+                        host['ip'] = response.src
+                        # guarda el tiempo obtenido en el host
+                        host['tiempos'].append(end_time - start_time)
+                        # aumenta contador
+                        self.numRep += 1
+                        # reseteamos el numero de intentos
+                        numIntento = 0
+                    # chequeamos si es el nodo de destino
+                    if response[ICMP].type == TYPE_LASTNODE:
+                        print "PASA POR SALIDA"
+                        fin_camino = True
+            if len(host['tiempos']) > 0:
+                last_ip = host['ip']
+                self.route.append(host)
+            self.ttlActual += 1
 
-    ej1 = False
-    arrayIP_RTT = []
-    for host in route:
-        # si es de red interna le digo a la pagina que me rastree
-        pais = ""
-        region = ""
-        ciudad = ""
-        latitud = ""
-        longitud = ""
-        respok = 0
-        intentos_geo = 0
-        while not respok and intentos_geo < 10:
-            response = requests.get("http://freegeoip.net/json/" + str(host['ip']))
-            if response.status_code == 200:
-                # parsea el json de respuesta de la api
-                infoip = json.loads(response.content)
-                # se guarda los datos necesarios
-                pais = infoip['country_name'].encode('utf-8')
-                region = infoip['region_name'].encode('utf-8')
-                ciudad = infoip['city'].encode('utf-8')
-                latitud = infoip['latitude']
-                longitud = infoip['longitude']
-                # setea que recibe los datos ok
-                respok = 1
-            intentos_geo += 1
-        if ej1:
+    def buscarIps(self):
+        for host in self.route:
+            # si es de red interna le digo a la pagina que me rastree
+            pais = ""
+            region = ""
+            ciudad = ""
+            latitud = ""
+            longitud = ""
+            respok = 0
+            intentos_geo = 0
+            while not respok and intentos_geo < 10:
+                response = requests.get("http://freegeoip.net/json/" + str(host['ip']))
+                if response.status_code == 200:
+                    # parsea el json de respuesta de la api
+                    infoip = json.loads(response.content)
+                    # se guarda los datos necesarios
+                    pais = infoip['country_name'].encode('utf-8')
+                    region = infoip['region_name'].encode('utf-8')
+                    ciudad = infoip['city'].encode('utf-8')
+                    latitud = infoip['latitude']
+                    longitud = infoip['longitude']
+                    # setea que recibe los datos ok
+                    respok = 1
+                intentos_geo += 1
+            # guarda en csv la salida
             for rtt in host['tiempos']:
-                logger.writerow([host['ttl'], host['ip'], rtt, pais, region, ciudad, latitud, longitud])
-        else:
-            tiempos = np.array(host['tiempos'])
-            arrayIP_RTT.append((host['ip'],np.mean(tiempos)))
+                self.loggerRoute.writerow([host['ttl'], host['ip'], rtt, pais, region, ciudad, latitud, longitud])
 
-    if not ej1: # cimbala
-        print arrayIP_RTT
-        for i in range(1,len(arrayIP_RTT)): # calculo cuanto tardo en ese tramo
-            arrayIP_RTT[i] = (arrayIP_RTT[i][0],arrayIP_RTT[i][1]-arrayIP_RTT[i-1][1])
+    def estadoDesdeCsv(self, namefile):
+        self.route = []
+        self.namefile = namefile
+        with open(self.namefile + '_route.csv', 'rb') as archivocsv:
+            lector = csv.reader(archivocsv)
+            self.ttlActual = -1
+            ip_actual = -1
+            primera_linea = 1
+            host = {}
+            for linea in lector:
+                if primera_linea:
+                    primera_linea = 0
+                else:
+                    if self.ttlActual != linea[0]:
+                        if self.ttlActual > 0:
+                            self.route.append(host)
+                        self.ttlActual = linea[0]
+                        host = {}
+                        host['tiempos'] = []
+                        host['ttl'] = self.ttlActual
+                        host['ip'] = linea[1]
+                    host['tiempos'].append(float(linea[2]))
+            self.route.append(host)
+            self.actualizarVistaProceso()
+
+    def actualizarVistaProceso(self):
+        os.system('clear')
+        print "-------------TRACEROUTE-------------"
+        print "Destino:       " + self.destino
+        print "TTL actual:    " + str(self.ttlActual)
+        print "Num captura:   " + str(self.numRep)
+        print "------------------------------------"
+        print ""
+        for host in self.route:
+            print str(host['ttl']) + " -> " + host['ip'] + " - " + str(promediarRtts(host['tiempos']))
+        print ""
+
+    def calcularOutliers(self):
+        arrayIP_RTT = []
+        # guarda rtt por salto
+        rtt_ultimo_nodo = 0
+        for host in self.route:
+            rtt_promedio = promediarRtts(host['tiempos'])
+            arrayIP_RTT.append((host['ip'], rtt_promedio - rtt_ultimo_nodo))
+            rtt_ultimo_nodo = rtt_promedio
+
         # paso 0
-        cantidad = len(arrayIP_RTT)
-        print cantidad
+        cantidad_nodos = len(arrayIP_RTT)
         # paso 1
         arrayIP_RTT_sorted = sorted(arrayIP_RTT,key=lambda x: x[1])
         candidatoMin = arrayIP_RTT_sorted[0]
         candidatoMax = arrayIP_RTT_sorted[len(arrayIP_RTT_sorted)-1]
-        print candidatoMin
-        print candidatoMax
         # paso 2
         np_rtt = []
         for par in arrayIP_RTT:
@@ -122,23 +165,56 @@ def traceroute(destino, outfile):
         print masSospechoso
         # paso 6
         # TODO: hardcodear la tabla o calcularla si estamos con tiempos
-        thompson = float(raw_input("Dame el thompson modificado con n="+str(cantidad)))
+        thompson = raw_input("Dame el thompson modificado con n="+str(cantidad_nodos))
         # paso 7
-        valorCritico = thompson*stdev
+        valorCritico = float(thompson) * stdev
         print valorCritico
         # paso 8
         if valorCritico < masSospechoso:
             print "Para llegar a la ip "+(candidatoMin[0] if mayor else candidatoMax[0])+" hay un salto intercontinental"
+        else:
+            print "No hay salto intercontinental"
+
+def promediarRtts(arregloRtt):
+    return np.mean(np.array(arregloRtt))
 
 if os.geteuid() != 0:
     print "Correlo con root chabon!"
     exit(1)
 
-if len(sys.argv) > 2:
-    host = sys.argv[1]
-    outfile = sys.argv[2]
-else:
-    host = raw_input("Pone la ip/url: ")
-    outfile = raw_input("Pone el archivo de salida de logs: ")
+def main():
+    if len(sys.argv) < 2:
+        print "-----TRACEROUTE-----"
+        print "Modos:"
+        print "1 -> Capturar:"
+        print "    Parametros:"
+        print "        1. Host"
+        print "        2. Nombre de salida"
+        print "2 -> Usar archivo de entrada"
+        print "        1. Nombre de entrada"
+        print ""
+        print "Ejemplos:"
+        print "   traceroute.py 1 cambridge.ac.uk cambridge"
+        print "   traceroute.py 2 cambridge"
+        return
 
-traceroute(host, outfile)
+    if sys.argv[1] == '1':
+        if len(sys.argv) > 3:
+            host = sys.argv[2]
+            namefile = sys.argv[3]
+        else:
+            host = raw_input("Pone la ip/url: ")
+            namefile = raw_input("Pone el nombre del archivo de salida de logs: ")
+        tr = traceroute(host, namefile)
+        tr.iniciar()
+        tr.buscarIps()
+    else:
+        if len(sys.argv) > 2:
+            namefile = sys.argv[2]
+        else:
+            namefile = raw_input("Pone el nombre del archivo de entrada: ")
+        tr = traceroute('', '')
+        tr.estadoDesdeCsv(namefile)
+        tr.calcularOutliers()
+
+main()
